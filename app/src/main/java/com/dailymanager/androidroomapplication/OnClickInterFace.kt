8d5657cc8d673d27e01a2e29c98f3c0321ecb354
dipankar108 <nathbd10@gmail.com>
2022-04-06 12:22:00 +0600

    view done with recycler view

---
+++ app/src/main/java/com/dailymanager/androidroomapplication/OnClickInterFace.kt
@@ -0,0 +1,7 @@
+package com.dailymanager.androidroomapplication
+
+import com.dailymanager.androidroomapplication.roomdatabase.User
+
+interface OnClickInterFace {
+    fun onItemClick(user: User)
+}
--- app/src/main/java/com/dailymanager/androidroomapplication/UserAdapter.kt
+++ app/src/main/java/com/dailymanager/androidroomapplication/UserAdapter.kt
@@ -3,14 +3,17 @@ package com.dailymanager.androidroomapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dailymanager.androidroomapplication.roomdatabase.User
 
 class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
-    var userInfoArray = listOf<User>()
+    private var userInfoArray = listOf<User>()
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val fName: TextView = itemView.findViewById(R.id.tv_user_fName)
+        val lName: TextView = itemView.findViewById(R.id.tv_user_lName)
+        val age: TextView = itemView.findViewById(R.id.tv_userAge)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
@@ -19,7 +22,10 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
         )
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-
+        val res = userInfoArray[position]
+        holder.fName.text = res.fName
+        holder.lName.text = res.lName
+        holder.age.text = res.age.toString()
     }
 
     override fun getItemCount() = userInfoArray.size
